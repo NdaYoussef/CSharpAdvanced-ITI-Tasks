fdad7537170cb0acce5c16e19c5e3bd285e2853d
NdaYoussef <ndayou99@gmail.com>
2026-02-02 16:00:59 +0200

    add first task of day 1, Indexers in C#

---
+++ Day1P1/PhoneBook.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1P1
+{
+    internal class PhoneBook
+    {
+        public string[] Names = new string[10]; // new string[3];
+        public int[] Numbers = new int[10];
+
+
+
+            public int this[string name]
+            {
+            get
+            {
+                for(int i = 0; i < Names.Length; i++)
+                {
+                    if (name == Names[i])
+                      return Numbers[i];
+                }
+                return -1;
+            }
+
+            set
+            {
+                for(int i = 0;i < Numbers.Length;i++)
+                {
+                    if (Names[i] is null)
+                    {
+                        Names[i] = name;
+                        Numbers[i] = value;
+                        return;
+                    }
+                    else if (name == Names[i])
+                    { 
+                        Numbers[i] = value;
+                        return;
+                    }
+                }
+            }
+            }
+
+        public string this[int number]
+        {
+            get
+            {
+                for (int i = 0; i < Numbers.Length; i++)
+                {
+                    if (number == Numbers[i])
+                        return Names[i];
+                }
+                return "not matched!";
+            }
+
+            set
+            {
+                for(int i = 0; i<Numbers.Length; i++)
+                { 
+                    if (number == Numbers[i])
+                    {
+                        if (number > 0)
+                        {
+                            Names[i] = value;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if(number > 0)
+                        {
+                            Numbers[i] = number;
+                            Names[i] = value;
+                            return;
+                        }
+                       
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Day1P1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day1P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook phoneBook1 = new PhoneBook();
+
+            //int phoneValue = phoneBook1["aa"] = 123;
+
+
+            //Console.WriteLine($"Value of entered Name = {phoneValue}");
+
+            //PhoneBook phoneBook2 = new PhoneBook();
+            string nameValue = phoneBook1[125] = "Nada";
+            //Console.WriteLine($"Value of entered Number: {nameValue}");
+
+            var nameLength = phoneBook1.Numbers.Length;
+            for(int i = 0;i< nameLength;i++)
+            {
+                Console.WriteLine($"{phoneBook1.Names[i]},{phoneBook1.Numbers[i]}");
+                   
+            }
+        }
+    }
+}
