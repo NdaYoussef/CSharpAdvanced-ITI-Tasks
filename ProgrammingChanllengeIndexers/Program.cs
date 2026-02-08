1c3f415082ef901ceec12931e6268e6f69874736
NdaYoussef <ndayou99@gmail.com>
2026-02-08 11:27:24 +0200

    add first programming challenges of indexers in c#

---
+++ ProgrammingChanllengeIndexers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ProgrammingChanllengeIndexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SecretSafe secret = new SecretSafe();
+
+            secret[0] = "first secret in array";
+            secret[1] = "second secret in array";
+            secret[2] = "third secret in array";
+
+            secret[10] = "secret #10";
+            Console.WriteLine($" return first index of array in string: {secret[0]}");
+            Console.WriteLine($" return second index of array in string: {secret[1]}");
+            Console.WriteLine($" return # index of array in string: {secret[10]}");
+
+
+          //  secret["secret1"] = 1;
+
+            Console.WriteLine($"index of first value in array is:{secret["secret1"]}");
+        }
+    }
+}
---
+++ ProgrammingChanllengeIndexers/SecretSafe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChanllengeIndexers
+{
+    internal class SecretSafe
+    {
+        string[] secrets = new string[10];
+
+
+        //safe[0] = "My Secret"
+        public string this[int index]
+        {
+            get
+            {
+                for (int i = 0; i < secrets.Length; i++)
+                {
+                    if (index == i)
+                        return secrets[i];
+                }
+                return "Access Denied: Invalid Vault Key";
+            }
+
+            set
+            {
+                for (int i = 0; i < secrets.Length; i++)
+                {
+                    if (index == i)
+                        secrets[index] = value;
+                }
+
+                return;
+
+            }
+        }
+
+        public int this[string secret]
+        {
+            get
+            {
+                for(int i = 0;i<secrets.Length;i++)
+                {
+                    if(secrets[i] == secret)
+                        return i;
+                }
+
+                return -1;
+            }
+
+            //  SecretSafe["secret1"] = 1; 
+
+
+            //set
+            //{
+            //    for (int i = 0; i < secrets.Length; i++)
+            //    {
+            //        if (secrets[i] == secret)
+            //            secrets[i] = value.ToString();
+            //    }
+            //}
+}
+        }
+    }
+
