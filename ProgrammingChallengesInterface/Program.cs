574a820aaa512f9a176b4ce5ce999679511ee1fd
NdaYoussef <ndayou99@gmail.com>
2026-02-08 13:51:52 +0200

    add interfcae challenge

---
+++ ProgrammingChallengesInterface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingChallengesInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
