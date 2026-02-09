bbf9cedb19b3a37cb2cc29b1694162046e59103a
NdaYoussef <ndayou99@gmail.com>
2026-02-09 15:03:50 +0200

    add task of var

---
+++ Day5P1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day5P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            var anon = new { id = 1 };
+            var test = Test.keyValuePairs();
+
+          //  Test.keyValuePairs(anon);
+            List<Dictionary<string, object>> keyValuePairs = new List<Dictionary<string, object>>();
+
+            Console.WriteLine(keyValuePairs);
+            Console.WriteLine(test);
+        }
+    }
+}
---
+++ Day5P1/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5P1
+{
+    internal class Test
+    {
+        public static List<Dictionary<string, object>> keyValuePairs()
+        {
+            return new List<Dictionary<string, object>>();
+        }
+    }
+}
