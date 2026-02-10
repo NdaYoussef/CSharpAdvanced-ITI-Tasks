383fca66f533b27d42ae615d13f5cf10902a1671
NdaYoussef <ndayou99@gmail.com>
2026-02-10 13:17:17 +0200

    add day6 linq functions

---
+++ Day6/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Hours { get; set; }
+        public int DeptId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
