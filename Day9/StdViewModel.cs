05ab8e2291bf3de98934c8b2ad9a795e49e8b485
NdaYoussef <ndayou99@gmail.com>
2026-02-14 15:28:37 +0200

    add view class and map it in ef core

---
+++ Day9/StdViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    [Keyless]
+    public class StdViewModel
+    {
+      //  public int Id { get; set; }
+        public string StudentName { get; set; }
+        public double StdGPA { get; set; }
+        public string CrsName { get; set; }
+
+    }
+}
