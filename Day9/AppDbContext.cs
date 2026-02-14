84f9082c098fd83566a38cca38de4a1a9af9fd0a
NdaYoussef <ndayou99@gmail.com>
2026-02-14 14:51:11 +0200

    add task day9 Types of loading

---
+++ Day9/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    public class AppDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source = NDA ; Initial Catalog = Day9ITI ; Integrated Security = true; Encrypt = true ; TrustServerCertificate = true;").UseLazyLoadingProxies();
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>(e => {
+               e.Property(e=>e.Name).HasMaxLength(50).IsRequired();
+               
+                e.HasOne(c=>c.Course)
+                .WithMany(c => c.Students)
+                .HasForeignKey(c=>c.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Course>(e => { 
+            e.Property(n=>n.Name).HasMaxLength(50).IsRequired();
+
+                e.HasMany(c => c.Students)
+                .WithOne(c => c.Course)
+                .HasForeignKey(c=>c.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            });
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            AppDbContext db = new AppDbContext();
+
+            Console.WriteLine("1===============================================");
+            //-1
+            var courses = db.Courses.ToList();
+            foreach (var crs in courses)
+            {
+                Console.WriteLine($"Crs Name: {crs.Name}");
+                foreach (var item in crs.Students)
+                {
+                    Console.WriteLine($"Students list: {item.Name}");
+                }
+            }
+
+            Console.WriteLine("2===============================================");
+            //-2
+
+
+        }
+    }
+}
