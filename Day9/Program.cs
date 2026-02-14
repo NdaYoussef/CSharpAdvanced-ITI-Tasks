150dda3681e365b7c1ad3824c219859ec76a27b1
NdaYoussef <ndayou99@gmail.com>
2026-02-14 15:28:07 +0200

    solve all required queries

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Day9
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day9
 {
     internal class Program
     {
@@ -21,8 +23,46 @@
 
             Console.WriteLine("2===============================================");
             //-2
+            var students = db.Students.Where(c => c.CourseId == 3).Take(5);
+
+            foreach(var student in students)
+            {
+                student.GPA += 0.5;
+                Console.WriteLine($"Student Name: {student.Name}, Student GPA: {student.GPA} ");
+            }
+
+            db.SaveChanges();
 
 
+            Console.WriteLine("3===============================================");
+            //-3
+            var studentsList = db.Students.Take(5).Skip(2);
+            foreach (var student in studentsList)
+            {
+                Console.WriteLine($"Std Name: {student.Name}");
+            }
+
+            Console.WriteLine("4===============================================");
+            //-4
+
+            var listStds = db.Students.FromSqlRaw($"exec sp_ALlStudents").ToList();
+            foreach(var student in listStds)
+            {
+                Console.WriteLine($"Std Name: {student.Name}, Student GPA: {student.GPA}");
+            }
+
+
+            //var StdsWithCrs = db.Students.FromSqlRaw($"select * from dbo.vw_StdWithCrs").ToList();
+            //foreach (var student in listStds)
+            //{
+            //    Console.WriteLine($"Std Name: {student.Name}, Student Course: {student.Course.Name}, Student GPA: {student.GPA}");
+            //}
+
+            var data = db.Set<StdViewModel>().ToList();
+            foreach (var student in data)
+            {
+                Console.WriteLine($"Std Name: {student.StudentName}, Course Name: {student.CrsName}, Student GPA: {student.StdGPA}");
+            }
         }
     }
 }
