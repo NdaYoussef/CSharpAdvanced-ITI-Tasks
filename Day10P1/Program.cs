368303431ce137e3d867db51016093789578dfc6
NdaYoussef <ndayou99@gmail.com>
2026-02-15 12:54:03 +0200

    add task day 10 of threads p#1

---
+++ Day10P1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Day10P1
+{
+    internal class Program
+    {
+       static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        public static void Sum()
+        {
+            Console.WriteLine($"outer Thread : {Thread.CurrentThread.ManagedThreadId}, Name: {Thread.CurrentThread.Name}, Status: {Thread.CurrentThread.ThreadState}, IsAlive: {Thread.CurrentThread.IsAlive}, IsBackground: {Thread.CurrentThread.IsBackground} ");
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(numbers[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ThreadStart myThread = Sum;
+           // thread.Start();
+
+            Thread thread = new Thread(myThread);
+            thread.Start();
+            Console.WriteLine($"Inner main Thread : {Thread.CurrentThread.ManagedThreadId}, Name: {Thread.CurrentThread.Name}, Status: {Thread.CurrentThread.ThreadState}, IsAlive: {Thread.CurrentThread.IsAlive}, IsBackground: {Thread.CurrentThread.IsBackground} ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            { 
+                    if(i%2 != 0)
+                    Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("End of Program");
+
+        }
+    }
+}
