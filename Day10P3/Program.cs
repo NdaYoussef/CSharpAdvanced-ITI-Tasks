c2ff75e1abbf79bfec431d1866925dbee507223f
NdaYoussef <ndayou99@gmail.com>
2026-02-16 12:48:09 +0200

    add day 10 p#3 of writing and reading files async

--- Day10P3/Program.cs
+++ Day10P3/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Day10P3
+﻿using System.Diagnostics;
+
+namespace Day10P3
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            // ================= Sequential =================
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Seqential.MakCoffee();
+            Seqential.ToastingBread();
+            Seqential.FryingEggs();
+
+            sw.Stop();
+            Console.WriteLine("=================================");
+            Console.WriteLine($"Sequential Time: {sw.ElapsedMilliseconds} ms");
+
+            // ================= Parallel =================
+            Stopwatch sw2 = Stopwatch.StartNew();
+
+            Task coffeeTask = Task.Run(() => Parallel.MakeCoffee());
+            Task breadTask = Task.Run(() => Parallel.ToastingBread());
+            Task eggTask = Task.Run(() => Parallel.FryingEggs());
+
+             Task.WhenAll(coffeeTask, breadTask, eggTask);
+
+            sw2.Stop();
+            Console.WriteLine("=================================");
+            Console.WriteLine($"Parallel Time: {sw2.ElapsedMilliseconds} ms");
         }
     }
-}
+    }
+
