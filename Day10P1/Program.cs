31fd3877e131ed1d5dec6d32cbc7ccd41e66cd7f
NdaYoussef <ndayou99@gmail.com>
2026-02-16 12:47:08 +0200

    add day 10 p#1 adding stop watch code

--- Day10P1/Program.cs
+++ Day10P1/Program.cs
@@ -2,35 +2,38 @@
 {
     internal class Program
     {
-       static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+       static int sum = 0;
+        static int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
         public static void Sum()
         {
             Console.WriteLine($"outer Thread : {Thread.CurrentThread.ManagedThreadId}, Name: {Thread.CurrentThread.Name}, Status: {Thread.CurrentThread.ThreadState}, IsAlive: {Thread.CurrentThread.IsAlive}, IsBackground: {Thread.CurrentThread.IsBackground} ");
 
-
             for (int i = 0; i < numbers.Length; i++)
             {
                 if (i % 2 == 0)
-                    Console.WriteLine(numbers[i]);
+                    sum+= numbers[i];   
             }
+            Console.WriteLine($"even sum: {sum}");
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
             ThreadStart myThread = Sum;
-           // thread.Start();
+            // thread.Start();
 
             Thread thread = new Thread(myThread);
             thread.Start();
             Console.WriteLine($"Inner main Thread : {Thread.CurrentThread.ManagedThreadId}, Name: {Thread.CurrentThread.Name}, Status: {Thread.CurrentThread.ThreadState}, IsAlive: {Thread.CurrentThread.IsAlive}, IsBackground: {Thread.CurrentThread.IsBackground} ");
 
             for (int i = 0; i < numbers.Length; i++)
-            { 
+            {
+                   
                     if(i%2 != 0)
-                    Console.WriteLine(numbers[i]);
-            }
+                    sum += numbers[i];
 
+            }
+            Console.WriteLine($"Odd Sum = {sum}");
             Console.WriteLine("End of Program");
 
         }
