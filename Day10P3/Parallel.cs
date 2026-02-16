55e390d5428c54a7edb21b2837ee09babe05be19
NdaYoussef <ndayou99@gmail.com>
2026-02-16 12:43:40 +0200

    add day 10 task of parallel ex

---
+++ Day10P3/Parallel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10P3
+{
+    public class Parallel
+    {
+        public static void MakeCoffee()
+        {
+            Console.WriteLine("Making Coffee Parallel");
+            Thread.Sleep(3000);
+           
+        }
+
+        public static void ToastingBread()
+        {
+            Console.WriteLine("Toasting Bread Parallel");
+            Thread.Sleep(2000);
+            
+        }
+
+        public static void FryingEggs()
+        {
+            Console.WriteLine("Frying Eggs Parallel");
+            Thread.Sleep(4000);
+          
+        }
+    }
+}
---
+++ Day10P3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day10P3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
