b5ea022ef12d6e66a358c97cd62cf095f42bafc4
NdaYoussef <ndayou99@gmail.com>
2026-02-16 12:44:17 +0200

    add day 10 task of sequential ex

---
+++ Day10P3/Seqential.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10P3
+{
+    public class Seqential
+    {
+        public static void MakCoffee()
+        {
+            Console.WriteLine("Making Coffee seq");
+        }
+
+        public static void ToastingBread()
+        {
+            Console.WriteLine("Toasting Bread seq");
+        }
+        public static void FryingEggs()
+        {
+            Console.WriteLine("Frying Eggs seq");
+        }
+
+        //Stopwatch sw = Stopwatch.StartNew();
+        //Console.WriteLine($"Time: {sw.ElapsedMilliseconds}ms");
+    }
+}
